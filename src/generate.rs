73222e2007523c06ce6a09440caa34a7fe83714d
Robert Albus <hello@robertalbus.com>
2022-04-18 13:06:00 -0600

    move wavetable generation into it's own module

---
+++ src/generate.rs
@@ -0,0 +1,24 @@
+use crate::wave_shape::pure;
+
+pub fn generate_sin(
+  wavetable_size: usize,
+  shape_fn: Option<Box<dyn Fn(f32)->f32>>
+) -> Vec<f32> {
+
+  let shape_fn = get_shape_fn(shape_fn);
+
+  let mut wavetable: Vec<f32> = Vec::with_capacity(wavetable_size);
+  for n in 0..wavetable_size {
+    let sample = (2. * std::f32::consts::PI * n as f32 / wavetable_size as f32).sin();
+    wavetable.push(shape_fn(sample));
+  }
+  
+  wavetable
+}
+
+fn get_shape_fn(might: Option<Box<dyn Fn(f32)->f32>>) -> Box<dyn Fn(f32)->f32> {
+  match might {
+    Some(shape_fn) => shape_fn,
+    None => Box::new(pure),
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,19 @@ use wavetable::WavetableOscillator;
 mod wave_shape;
 use wave_shape::{bit_crush, square, pure, something};
 
+mod generate;
+use generate::generate_sin;
+
 
 fn main() {
     let sample_rate = 48_000;
     let playback_duration = Duration::from_secs(10);
     let freq = 40.;
-    let shape_fn = something;
+    let shape_fn = square;
+    let shape_fn: Option<Box<dyn Fn(f32)->f32>> = Some(Box::new(shape_fn));
     let wavetable_size = 1048;
 
-    let mut wavetable: Vec<f32> = Vec::with_capacity(wavetable_size);
-    for n in 0..wavetable_size {
-        let sample = (2. * std::f32::consts::PI * n as f32 / wavetable_size as f32).sin();
-        wavetable.push(shape_fn(sample));
-    }
+    let wavetable = generate::generate_sin(wavetable_size, shape_fn);
 
     let mut oscillator = WavetableOscillator::new(sample_rate, wavetable); 
     oscillator.set_frequency(freq);
@@ -33,3 +33,4 @@ fn main() {
 
     std::thread::sleep(playback_duration);
 }
+
