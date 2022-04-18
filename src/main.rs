8884ec5044d627c1dca92ed72df0084ec6bc89af
Robert Albus <hello@robertalbus.com>
2022-04-18 01:45:17 -0600

    tidy the code

--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,28 @@ mod wavetable;
 use wavetable::WavetableOscillator;
 
 mod wave_shape;
-use wave_shape::{bit_crush, square};
+use wave_shape::{bit_crush, square, pure};
 
 
 fn main() {
-    let wavetable_size = 64;
-    let mut wavetable: Vec<f32> = Vec::with_capacity(wavetable_size);
-    let shape_fn = bit_crush(1.2);
+    let sample_rate = 48_000;
+    let playback_duration = Duration::from_secs(10);
+    let freq = 40.;
+    let shape_fn = pure;
+    let wavetable_size = 1048;
 
+    let mut wavetable: Vec<f32> = Vec::with_capacity(wavetable_size);
     for n in 0..wavetable_size {
         let sample = (2. * std::f32::consts::PI * n as f32 / wavetable_size as f32).sin();
         wavetable.push(shape_fn(sample));
     }
 
-    let mut oscillator = WavetableOscillator::new(48_000, wavetable); 
-    oscillator.set_frequency(55.);
+    let mut oscillator = WavetableOscillator::new(sample_rate, wavetable); 
+    oscillator.set_frequency(freq);
 
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 
     let _result = stream_handle.play_raw(oscillator.convert_samples());
 
-    std::thread::sleep(Duration::from_secs(5));
+    std::thread::sleep(playback_duration);
 }
