358805307d0ac0c0ce6ab8791f683e9d4c0b2441
Robert Albus <hello@robertalbus.com>
2022-04-24 17:54:31 -0600

    run main program until keydown

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 
-use std::time::Duration;
+use console::Term;
 use rodio::{OutputStream, Source};
+use std::time::Duration;
+
 mod wavetable;
 use wavetable::WavetableOscillator;
 
 mod wave_shape;
-use wave_shape::{bit_crush, square, pure, something, shape, gain, clip};
+use wave_shape::{bit_crush, clip, gain, pure, shape, something, square};
 
 mod generate;
 use generate::sin;
@@ -17,32 +19,32 @@ mod visualize;
 
 fn main() {
     let sample_rate = 48_000;
-    let playback_duration = Duration::from_secs(10);
     let freq = 40.;
     let wavetable_size = 2048;
-    
+
     let mut wavetable = generate::sin(wavetable_size);
 
     for _ in 0..10000 {
-        shape(&mut wavetable, vec![
-            gain(1.1), 
-            clip(0.95),
-            bit_crush(22.),
-            ]
-        );
+        shape(&mut wavetable, vec![gain(1.1), clip(0.95), bit_crush(22.)]);
     }
 
     shape(&mut wavetable, vec![gain(0.1)]);
 
     visualize::png(&wavetable, "wave.png");
 
-    let mut oscillator = WavetableOscillator::new(sample_rate, wavetable); 
+    let mut oscillator = WavetableOscillator::new(sample_rate, wavetable);
     oscillator.set_frequency(freq);
 
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 
     let _result = stream_handle.play_raw(oscillator.convert_samples());
 
-    std::thread::sleep(playback_duration);
+    let stdout = Term::buffered_stdout();
+    'program_loop: loop {
+        if let Ok(character) = stdout.read_char() {
+            match character {
+                _ => break 'program_loop,
+            }
+        }
+    }
 }
-
